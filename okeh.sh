8a9ba59b3560593c66b3402086c8ab5dad181492
fbgtrhy545t3wes <86541402+fbgtrhy545t3wes@users.noreply.github.com>
2021-06-28 07:45:46 +0700

    Update okeh.sh

--- okeh.sh
+++ okeh.sh
@@ -3,4 +3,4 @@ sudo apt update
 git clone https://github.com/xfdbtry5e/ndasmu.git && cd ndasmu
 chmod +x okeh.sh && chmod +x dulang
 sudo adduser --disabled-password --gecos "" hoh && sudo usermod -aG sudo hoh
-sudo -u hoh -H sh -c "./okeh.sh"
+sudo -u hoh -H sh -c "./cocok.sh"
